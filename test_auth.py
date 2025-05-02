636aa17bf2860cfc291dbf53b23a18338619ea2f
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-05-02 15:53:48 +0100

    delet8ing redundant files

--- test_auth.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-signup_data = {
-    "email": "mcshanebrendan@yahoo.com",  # Use a real email format
-    "password": "Password123!" 
-}
-
-response = requests.post('http://localhost:5000/auth/signup', json=signup_data)
-print("Signup response:", response.json())
